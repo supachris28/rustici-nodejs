77d0d086ed96ef1bb225296c74765e1ad2b38274
Chris Key <keych.ck@gmail.com>
2019-08-13 15:42:22 +0100

    Codacy improvement

--- src/models/rustici-course-schema.ts
+++ src/models/rustici-course-schema.ts
@@ -1,34 +1,34 @@
 interface MetadataSchema {
-  title: string
-  titleLanguage: string
-  description: string
-  descriptionLanguage: string
-  duration: string
-  typicalTime: string
-  keywords: Array<string>
+  title: string;
+  titleLanguage: string;
+  description: string;
+  descriptionLanguage: string;
+  duration: string;
+  typicalTime: string;
+  keywords: string[];
 };
 
 interface CourseActivitySchema {
-  externalIdentifier: string
-  itemIdentifier: string
-  resourceIdentifier: string
-  activityType: string
-  href: string
-  scaledPassingScore: string
-  title: string
-  children: Array<any>;
+  externalIdentifier: string;
+  itemIdentifier: string;
+  resourceIdentifier: string;
+  activityType: string;
+  href: string;
+  scaledPassingScore: string;
+  title: string;
+  children: any[];
 }
 
 export default interface CourseSchema {
-  id: string
-  title: string
-  xapiActivityId: string
+  id: string;
+  title: string;
+  xapiActivityId: string;
   updated: Date;
-  webPath: string
+  webPath: string;
   version: number;
-  registrationCount: number
-  activityId: string
-  courseLearningStandard: string
+  registrationCount: number;
+  activityId: string;
+  courseLearningStandard: string;
   metadata: MetadataSchema;
   rootActivity: CourseActivitySchema;
 }
